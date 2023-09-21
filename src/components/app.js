7cd0ea6274a391aacc654d0577139d843887513d
SoniaPL22 <Sonia.petite.98@gmail.com>
2023-09-21 14:08:59 +0200

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Manualidades ANA</h1>
+        <h2>Fofuchas y demás</h2>
+      </div>
+    );
+  }
+}
