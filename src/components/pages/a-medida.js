13950d01d0d77296cb6cee77aedd0c65f9b605f6
SoniaPL22 <Sonia.petite.98@gmail.com>
2023-10-22 00:54:24 +0200

    enlaces mejorados

--- src/components/pages/a-medida.js
+++ src/components/pages/a-medida.js
@@ -10,7 +10,7 @@
                 <div className='amedida-elements-wrapper'>
                     <h2>Puedes cambiar los <span className='text-amedida'>detalles y los colores</span> de la manualidad que quieras.</h2>
                     <h2>Si nos mandas un <span className='text-amedida'>email</span> con los detalles y/o colores que quieres cambiar a la manualidad podremos <span className='text-amedida'>personalizartela a tu gusto</span>.</h2>
-                    <button className='btn page-amedida'><a href='/contacto' className='button-link-amedida'>Mandar un email con detalles y colores escogidos</a></button>
+                    <a href='/contacto' className='button-link-amedida'><button className='btn page-amedida'>Mandar un email con detalles y colores escogidos</button></a>
                     <h2>También tienes la opción de escoger la <span className='text-amedida'>fofucha personalizada</span>. </h2>
                     <h2>La fofucha personalizada tendrá rasgos más parecidos a la persona a diferencia de la fofucha clásica cuya cara será mas sencilla.</h2>
                     <h2>Aquí vemos la diferencia entre una <span className='text-amedida'>fofucha personalizada</span> y una <span className='text-amedida'>fofucha clásica</span>.</h2>
@@ -27,7 +27,7 @@
                     </div>
                     <h2>Si tienes alguna duda mandanos un email.</h2>
 
-                    <button className='btn page-amedida'><a className='button-link-amedida' href='/contacto'>Ir a mandar un email</a></button>
+                    <a className='button-link-amedida' href='/contacto'><button className='btn page-amedida'>Ir a mandar un email</button></a>
                 </div>
              </div>
          );
