e1369f0a93c99895fbb49edbe4742c56e855908c
SoniaPL22 <Sonia.petite.98@gmail.com>
2023-10-22 00:06:53 +0200

    home changes

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -9,15 +9,15 @@ export default class Inicio extends Component {
             <div>
                 
                 <div className='home-page-wrapper'>
-                    <h1 className='home-page-title'>Bienvenidos a la <span className='name-shop'>Fabrica Fofuchera</span></h1>
-                    <h2 className='home-page-description'>Aqui encontrarás fofuchas, amigurumis y muchas manualidades más</h2>
+                    <h1 className='home-page-title'>Bienvenidos a la <span className='name-shop'>Fábrica Fofuchera</span></h1>
+                    <h2 className='home-page-description'>Aquí encontrarás fofuchas, amigurumis y muchas manualidades más</h2>
                     <div className='offers-wrapper'>
                         <div className='two-offers-wrapper'>
                             <div className='offer offer-one'>
                                <h2 className='offer-text'>¡¡Si compras <span className='offer-detail'>2 fofuchas</span> te llevas un  <span className='offer-detail'>amigurumi de regalo</span>!!</h2>
                             </div>
                             <div className='offer offer-two'>
-                                <h2 className='offer-text'>¡¡Lote de <span className='offer-detail'>tres amigurumis</span> a elegir por tan solo <span className='offer-detail'>20 €</span>!!</h2>
+                                <h2 className='offer-text'>¡¡Lote de <span className='offer-detail'>tres amigurumis</span> a elegir por tan sólo <span className='offer-detail'>20 €</span>!!</h2>
                             </div>
                         </div>
                             <div className='offer offer-three'>
